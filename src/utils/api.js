a88515a48c7e04fa058881da091097320687b379
Koptay Law Firm <info@koptay.av.tr>
2025-10-04 19:15:32 +0300

    Fix image URL concatenation issue - remove STRAPI_BASE_URL from image URLs

--- src/utils/api.js
+++ src/utils/api.js
@@ -27,7 +27,7 @@ const getArticlesFromStrapi = async () => {
         metaDescription: article.seoDescription || article.excerpt,
         metaKeywords: article.keywords || '',
         seoTitle: article.seoTitle || article.title,
-        image: article.image?.url ? `${STRAPI_BASE_URL}${article.image.url}` : '/images/hero.jpg'
+        image: article.image?.url || '/images/hero.jpg'
       }))
     }
   } catch (error) {
